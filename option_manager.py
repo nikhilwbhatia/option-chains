a1d3acb7f7852822b6121f833c6e2c9a0f65b189
Shrey Bhatia <Shrey@sheungs-imac.lan>
2021-11-06 11:31:24 -0700

    Initial commit, add notebook and module

---
+++ option_manager.py
@@ -0,0 +1,14 @@
+import pyetrade
+
+def get_option_info(
+    consumer_key: str,
+    consumer_secret: str,
+    oauth_token: str,
+    oath_secret: str,
+    ticker: str,
+    min_percent: float,
+    max_percent: float,
+    increment: float
+):
+    print("hi")
+    
